1c49d9ea00eb507472a001eace06dc718d9c58d6
Your Name <you@example.com>
2023-08-21 17:40:13 +0530

    initial commit

---
+++ src/index.php
@@ -0,0 +1,16 @@
+<?php
+    $host = "mysql_db";
+    $username = "root";
+    $password = "root";
+    $db_name = "StudentDB";
+
+    // Create connection
+    $conn = mysqli_connect($host,$username, $password,$db_name);
+    //$con = new mysqli('mysql_db','root','root','studentdb');
+    if(!$conn){
+        die("Connection failed: ".mysqli_connect_error());
+    }
+    else{
+        echo "Connected Succesfully to the database";
+    }
+?>
